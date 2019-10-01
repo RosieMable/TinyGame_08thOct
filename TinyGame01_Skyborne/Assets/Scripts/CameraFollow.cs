16f3cceff66849d02da00e2d516bc873e5b85da5
Peter <peterchampion1997@gmail.com>
2019-10-01 14:30:06 +0100

    Implemented CameraFollow script
    Allows for the camera to follow the players movement, may need to be adjusted to lerp to the players position instead of directly altering transform.position of the camera.

---
+++ TinyGame01_Skyborne/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().gameObject; // Finds reference to the player through finding which gameObject has the 'PlayerMovement' class attached. 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
